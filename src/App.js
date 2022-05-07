ba97d2d53d1e91ff5bedce4bb25b11107bb4485b
Marcos Norberto <norbrj@gmail.com>
2022-05-07 20:17:32 -0300

    teste

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and   todad.
+          Edit <code>src/App.js</code> and   to22dad.
         </p>
         <a
           className="App-link"
